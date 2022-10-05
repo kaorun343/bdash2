21cfb5d9e939a4f8a3ea6e72446f8c49b1153e5d
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-10-05 09:04:08 +0900

    Add GlobalMenu component

---
+++ packages/renderer/src/components/GlobalMenu.tsx
@@ -0,0 +1,30 @@
+import { FC, PropsWithChildren } from 'react'
+import { FaTerminal } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
+
+const NavLinkItem: FC<PropsWithChildren<{ to: string }>> = ({ to, children }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `w-full h-full grid justify-items-center items-center ${isActive ? 'bg-gray-600' : undefined} hover:bg-gray-900`
+      }
+    >
+      {children}
+    </NavLink>
+  )
+}
+
+export const GlobalMenu: FC = () => {
+  return (
+    <nav className="w-16 h-screen bg-gray-800">
+      <ul>
+        <li className="block text-center text-3xl h-16">
+          <NavLinkItem to="/queries">
+            <FaTerminal className="text-white" />
+          </NavLinkItem>
+        </li>
+      </ul>
+    </nav>
+  )
+}
--- packages/renderer/src/components/Layout.tsx
+++ packages/renderer/src/components/Layout.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { GlobalMenu } from './GlobalMenu'
 
 export const Layout: FC = () => {
   return (
-    <div>
+    <div className="flex">
+      <aside>
+        <GlobalMenu />
+      </aside>
       <Outlet />
     </div>
   )
