7dd1ea793ed2f82924917ad6ccbad9c01d9f191a
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-10-07 21:42:31 +0900

    Fix Queries component

--- packages/renderer/src/features/queries/routes/Queries.tsx
+++ packages/renderer/src/features/queries/routes/Queries.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react'
 import { Outlet, useLoaderData } from 'react-router-dom'
+import { GetUserQueriesQuery } from '~/lib/graphql/generated'
 import { QueryList } from '../components/QueryList'
-import { GetQueriesQuery } from './index.generated'
 
 export const Queries: FC = () => {
-  const data = useLoaderData() as GetQueriesQuery
+  const data = useLoaderData() as GetUserQueriesQuery
 
   return (
     <div className="flex">
-      <QueryList queries={data.queries} />
+      <QueryList queries={data.userQueries} />
       <Outlet />
     </div>
   )
