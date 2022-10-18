073a863b0fec1229ce7d83c1b7171d111f83c469
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-10-19 00:22:31 +0900

    Use react-hook-form

--- packages/renderer/src/features/queries/components/query/QueryDetailHeader.tsx
+++ packages/renderer/src/features/queries/components/query/QueryDetailHeader.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react'
+import { UseFormRegister } from 'react-hook-form'
+import { GetUserQueryQuery } from '~/lib/graphql/generated'
 
 type Props = {
-  value: string
-  onChange(value: string): void
+  register: UseFormRegister<GetUserQueryQuery['userQuery']>
 }
 
-export const QueryDetailHeader: FC<Props> = ({ value, onChange }) => {
+export const QueryDetailHeader: FC<Props> = ({ register }) => {
   return (
     <div className="w-full border-b border-gray-300 p-2">
-      <input className="font-bold text-2xl" type="text" value={value} onChange={(e) => onChange(e.target.value)} />
+      <input className="font-bold text-2xl" type="text" {...register('title')} />
     </div>
   )
 }
--- packages/renderer/src/features/queries/routes/Query.tsx
+++ packages/renderer/src/features/queries/routes/Query.tsx
@@ -1,6 +1,9 @@
-import { FC, useCallback } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import produce from 'immer'
+import { FC, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { useLoaderData } from 'react-router-dom'
-import { GetUserQueryQuery, Sdk } from '~/lib/graphql/generated'
+import { GetUserQueriesQuery, GetUserQueryQuery, Sdk } from '~/lib/graphql/generated'
 import { QueryDetailHeader } from '../components/query/QueryDetailHeader'
 
 type Props = {
@@ -8,13 +11,39 @@ type Props = {
 }
 
 export const Query: FC<Props> = ({ sdk }) => {
+  const queryClient = useQueryClient()
   const data = useLoaderData() as GetUserQueryQuery
+  const id = data.userQuery.id
+  const { register, watch } = useForm({ defaultValues: data.userQuery })
 
-  const handleChangeTitle = useCallback((value: string) => {}, [])
+  const updateUserQueryTitleMutation = useMutation(async (title: string) => title, {
+    onSuccess: (title) => {
+      queryClient.setQueriesData<GetUserQueriesQuery>(
+        ['getUserQueries'],
+        produce((draft) => {
+          if (!draft) return
+          const query = draft.userQueries.find((query) => query.id === id)
+          if (!query) return
+          query.title = title
+        })
+      )
+    },
+  })
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      if (name !== 'title') return
+      if (type !== 'change') return
+      if (typeof value.title !== 'string') return
+      updateUserQueryTitleMutation.mutate(value.title)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [id, watch])
 
   return (
     <div className="flex-1">
-      <QueryDetailHeader value={data.userQuery.title} onChange={handleChangeTitle} />
+      <QueryDetailHeader register={register} />
     </div>
   )
 }
