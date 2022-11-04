ede0fee965624b55d0bf0935d5cd841e209b22f6
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-05 03:12:40 +0900

    Update Queries and QueryList components

--- packages/renderer/src/features/queries/components/queries/QueryList.tsx
+++ packages/renderer/src/features/queries/components/queries/QueryList.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { FC } from 'react'
 import { GetUserQueriesQuery, Sdk } from '~/lib/graphql/generated'
-import { QueryListHeader } from './QueryListHeader'
 import { QueryListItem } from './QueryListItem'
 
 type Props = {
@@ -12,13 +11,10 @@ export const QueryList: FC<Props> = ({ sdk }) => {
   const { data } = useQuery<GetUserQueriesQuery>(['getUserQueries'], () => sdk.getUserQueries())
 
   return (
-    <div className="h-screen w-72 border-r border-gray-300">
-      <QueryListHeader sdk={sdk} />
-      <ul>
-        {data?.userQueries.map((query) => (
-          <QueryListItem key={query.id} query={query} />
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {data?.userQueries.map((query) => (
+        <QueryListItem key={query.id} query={query} />
+      ))}
+    </ul>
   )
 }
--- packages/renderer/src/features/queries/routes/Queries.tsx
+++ packages/renderer/src/features/queries/routes/Queries.tsx
@@ -2,6 +2,7 @@ import { FC, Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Sdk } from '~/lib/graphql/generated'
 import { QueryList } from '../components/queries/QueryList'
+import { QueryListHeader } from '../components/queries/QueryListHeader'
 
 type Props = {
   sdk: Sdk
@@ -10,7 +11,10 @@ type Props = {
 export const Queries: FC<Props> = ({ sdk }) => {
   return (
     <div className="flex w-full">
-      <QueryList sdk={sdk} />
+      <div className="h-screen w-72 border-r border-gray-300">
+        <QueryListHeader sdk={sdk} />
+        <QueryList sdk={sdk} />
+      </div>
       <Suspense>
         <Outlet />
       </Suspense>
