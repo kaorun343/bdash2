47b34df36b43f6af01cd0ea689b5351932580c94
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-20 03:01:29 +0900

    Refactor QueryDetail component

--- packages/renderer/src/features/queries/components/query/QueryDetailEditor.tsx
+++ packages/renderer/src/features/queries/components/query/QueryDetailEditor.tsx
@@ -1,18 +1,56 @@
 import { sql } from '@codemirror/lang-sql'
 import { EditorView } from '@codemirror/view'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { basicSetup } from 'codemirror'
+import produce from 'immer'
 import { FC, useEffect, useRef } from 'react'
-import { UseFormGetValues, UseFormSetValue, UseFormWatch } from 'react-hook-form'
-import { GetUserQueryQuery } from '~/lib/graphql/generated'
+import { useForm } from 'react-hook-form'
+import { GetUserQueriesQuery, GetUserQueryQuery, Sdk } from '~/lib/graphql/generated'
 
 type Props = {
-  getValues: UseFormGetValues<GetUserQueryQuery['userQuery']>
-  setValue: UseFormSetValue<GetUserQueryQuery['userQuery']>
-  watch: UseFormWatch<GetUserQueryQuery['userQuery']>
+  data: GetUserQueryQuery
+  groupId: string
+  sdk: Sdk
 }
 
-export const QueryDetailEditor: FC<Props> = ({ getValues, setValue, watch }) => {
+export const QueryDetailEditor: FC<Props> = ({ data, groupId, sdk }) => {
   const containerRef = useRef<HTMLDivElement>(null)
+  const queryClient = useQueryClient()
+  const id = data.userQuery.id
+  const { register, getValues, setValue, watch } = useForm({ defaultValues: data.userQuery })
+
+  useEffect(() => {
+    setValue('title', data.userQuery.title)
+    setValue('body', data.userQuery.body)
+  }, [data.userQuery])
+
+  const updateUserQueryTitleMutation = useMutation(
+    async (title: string) => sdk.updateUserQueryTitle({ input: { id, title } }),
+    {
+      onSuccess: (result) => {
+        queryClient.setQueriesData<GetUserQueriesQuery>(
+          ['getUserQueries', groupId],
+          produce((draft) => {
+            if (!draft) return
+            const query = draft.userQueriesByGroup.find((query) => query.id === id)
+            if (!query) return
+            query.title = result.updateUserQueryTitle.userQuery.title
+          })
+        )
+      },
+    }
+  )
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      if (name !== 'title') return
+      if (type !== 'change') return
+      if (typeof value.title !== 'string') return
+      updateUserQueryTitleMutation.mutate(value.title)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [id, watch])
 
   useEffect(() => {
     const container = containerRef.current
@@ -45,5 +83,12 @@ export const QueryDetailEditor: FC<Props> = ({ getValues, setValue, watch }) => 
     }
   }, [getValues, setValue, watch])
 
-  return <div ref={containerRef} />
+  return (
+    <div className="w-full">
+      <div className="w-full border-b border-gray-300 p-2">
+        <input className="font-bold text-2xl" type="text" {...register('title')} />
+      </div>
+      <div ref={containerRef} />
+    </div>
+  )
 }
