d438d7ecedb0aeb73c7704169ec6ca2574858284
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-19 23:00:21 +0900

    Add QueryGroupList component

--- packages/renderer/src/features/queries/routes/QueryGroups.tsx
+++ packages/renderer/src/features/queries/routes/QueryGroups.tsx
@@ -1,10 +1,21 @@
-import { FC } from 'react'
+import { FC, Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
 import { Sdk } from '~/lib/graphql/generated'
+import { QueryGroupList } from '../components/queryGroups/QueryGroupList'
 
 type Props = {
   sdk: Sdk
 }
 
-export const QueryGroups: FC<Props> = () => {
-  return null
+export const QueryGroups: FC<Props> = ({ sdk }) => {
+  return (
+    <div className="flex w-full">
+      <div className="h-screen w-72 border-r border-gray-300">
+        <QueryGroupList sdk={sdk} />
+      </div>
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </div>
+  )
 }
