aff253f39a8dfeb698c059b9a9a45e09d28cc945
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-20 00:48:23 +0900

    Update loader function

--- packages/renderer/src/features/queries/routes/index.tsx
+++ packages/renderer/src/features/queries/routes/index.tsx
@@ -11,7 +11,7 @@ export const createQueryRoutes = (sdk: Sdk): RouteObject => {
   return {
     path: '/query-groups',
     element: <QueryGroups sdk={sdk} />,
-    loader: () => queryClient.fetchQuery(['getUserQueryGroups'], () => sdk.getUserQueryGroups()),
+    loader: () => sdk.getUserQueryGroups(),
     children: [
       {
         path: ':queryGroupId',
