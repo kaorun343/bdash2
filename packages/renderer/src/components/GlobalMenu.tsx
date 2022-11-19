ae8e2e7cdc16ded52b9fccefad989f9f1efecb9a
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-20 03:36:36 +0900

    Add data-source menu item

--- packages/renderer/src/components/GlobalMenu.tsx
+++ packages/renderer/src/components/GlobalMenu.tsx
@@ -1,17 +1,19 @@
 import { FC, PropsWithChildren } from 'react'
-import { FaTerminal } from 'react-icons/fa'
+import { FaDatabase, FaTerminal } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 
 const NavLinkItem: FC<PropsWithChildren<{ to: string }>> = ({ to, children }) => {
   return (
-    <NavLink
-      to={to}
-      className={({ isActive }) =>
-        `w-full h-full grid justify-items-center items-center ${isActive ? 'bg-gray-600' : undefined} hover:bg-gray-900`
-      }
-    >
-      {children}
-    </NavLink>
+    <li className="block text-center text-3xl h-16">
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          `w-full h-full grid justify-items-center items-center ${isActive ? 'bg-gray-600' : ''} hover:bg-gray-900`
+        }
+      >
+        {children}
+      </NavLink>
+    </li>
   )
 }
 
@@ -19,11 +21,12 @@ export const GlobalMenu: FC = () => {
   return (
     <nav className="w-16 h-screen bg-gray-800">
       <ul>
-        <li className="block text-center text-3xl h-16">
-          <NavLinkItem to="/query-groups">
-            <FaTerminal className="text-white" />
-          </NavLinkItem>
-        </li>
+        <NavLinkItem to="/query-groups">
+          <FaTerminal className="text-white" />
+        </NavLinkItem>
+        <NavLinkItem to="/data-sources">
+          <FaDatabase className="text-white" />
+        </NavLinkItem>
       </ul>
     </nav>
   )
