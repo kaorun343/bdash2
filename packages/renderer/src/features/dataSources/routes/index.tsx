7bd82907b767194c8bb8a868dc0c941c865b7bcd
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2022-11-23 15:47:20 +0900

    Add TableListPage component

--- packages/renderer/src/features/dataSources/routes/index.tsx
+++ packages/renderer/src/features/dataSources/routes/index.tsx
@@ -3,11 +3,18 @@ import { RouteObject } from 'react-router-dom'
 import { Sdk } from '~/lib/graphql/generated'
 
 const DataSourceListPage = lazy(() => import('./DataSourceListPage').then((m) => ({ default: m.DataSourceListPage })))
+const TableListPage = lazy(() => import('./TableListPage').then((m) => ({ default: m.TableListPage })))
 
 export const createDataSourceRoutes = (sdk: Sdk): RouteObject => {
   return {
     path: '/data-sources',
     element: <DataSourceListPage sdk={sdk} />,
     loader: () => sdk.dataSourceListPage(),
+    children: [
+      {
+        path: ':dataSourceId',
+        element: <TableListPage sdk={sdk} />,
+      },
+    ],
   }
 }
