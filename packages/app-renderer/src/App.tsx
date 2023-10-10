2e09608b9e7aa65a83cb89dd32aa185a968b8053
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2023-10-10 12:08:04 +0900

    Remove @tanstack/react-query

--- packages/app-renderer/src/App.tsx
+++ packages/app-renderer/src/App.tsx
@@ -1,17 +1,11 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import { FC } from 'react'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import { queryClient } from './lib/queryClient'
 
 type Props = {
   router: ReturnType<typeof createHashRouter>
 }
 
 export const App: FC<Props> = ({ router }) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  )
+  return <RouterProvider router={router} />
 }
