626aaea69067800055b9c0567d5954ad043af707
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2023-10-12 14:13:51 +0900

    Stop using node api in vite.config.js

--- packages/app-main/vite.config.js
+++ packages/app-main/vite.config.js
@@ -1,4 +1,3 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -7,7 +6,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      graphql: resolve(__dirname, '../../node_modules/graphql/index.js'),
+      graphql: './node_modules/graphql/index.js',
     },
   },
 })
