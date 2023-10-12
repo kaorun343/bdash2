125ee5519a8e4670dc455db01696c1e7f608a132
kaorun343 <5625395+kaorun343@users.noreply.github.com>
2023-10-12 11:37:52 +0900

    Add test of Query.dataSources

---
+++ packages/app-main/tests/resolvers/Query/dataSources.spec.ts
@@ -0,0 +1,55 @@
+import { Kysely } from 'kysely'
+import initSqlJs from 'sql.js'
+import { Database } from '../../../src/types'
+import { createDatabase } from '../../../src/database'
+import { migrateToLatest } from '../../../src/migrations'
+import { graphql, print } from 'graphql'
+import gql from 'graphql-tag'
+import { schema } from '../../../src/schema'
+
+const source = print(gql`
+  query getDataSources {
+    dataSources {
+      id
+      name
+      config {
+        __typename
+        ... on DataSourceConfigSQLite3 {
+          path
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`)
+
+const SQL = await initSqlJs()
+let db: Kysely<Database>
+
+beforeEach(async () => {
+  db = createDatabase(new SQL.Database())
+  await migrateToLatest(db)
+
+  await db
+    .insertInto('dataSource')
+    .values({
+      name: 'test',
+      type: 'sqlite3',
+      config: JSON.stringify({ path: '/path/to/sqlite3.database' }),
+      createdAt: '2021-01-01 00:00:00',
+      updatedAt: '2021-01-01 00:00:00',
+    })
+    .execute()
+})
+
+describe('Query.dataSources', () => {
+  it('should return all data sources', async () => {
+    const result = await graphql({
+      schema,
+      source,
+      contextValue: { db },
+    })
+    expect(result).toMatchSnapshot()
+  })
+})
